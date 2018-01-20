dcc2e977a1a3a0b535dcb18f0c55dfbbfe265201
rishabh kalra <rishabhkalra96@gmail.com>
2018-01-21 03:14:10 +0530

    added template for reset-password

--- accounts/urls.py
+++ accounts/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
     url(r'^change-password/$', views.change_password, name='change_password'),
-    url(r'^reset-password/$', password_reset, name='password_reset'),
+    url(r'^reset-password/$', password_reset,{'template_name': 'accounts/reset_password.html'}, name='password_reset'),
     url(r'^reset-password/done/$', password_reset_done, name='password_reset_done'),
     url(r'^reset-password/confirm/$', password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
