025ffd5a8d7fdaa46435c31c9255836247a20637
rishabh kalra <rishabhkalra96@gmail.com>
2018-01-28 17:36:01 +0530

    more customisation to admin userprofile display (column view)

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from accounts.models import UserProfile
 
 # Register your models here.
-admin.site.register(UserProfile)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'Information', 'phone', 'website_owned', 'city')
+
+    def Information(self, obj):
+        return obj.description
+
+    def website_owned(self, obj):
+        return obj.website
+    
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
