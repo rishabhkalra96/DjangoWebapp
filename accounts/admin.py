d8dc747cda4926e1686c98a66f996f22ea90c28c
rishabh kalra <rishabhkalra96@gmail.com>
2018-01-28 17:52:10 +0530

    more changes in accordance with previous commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,7 @@ from accounts.models import UserProfile
 
 # Register your models here.
 
+
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'Information', 'phone', 'website_owned', 'city')
 
@@ -12,6 +13,11 @@ class UserProfileAdmin(admin.ModelAdmin):
     def website_owned(self, obj):
         return obj.website
     
+    def get_queryset(self, request):
+        queryset = super(UserProfileAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('user')
+        return queryset
+
 admin.site.register(UserProfile, UserProfileAdmin)
 
 
