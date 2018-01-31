f5418ca65bb18f44bcb5256740ccb90ef13002ba
rishabh kalra <rishabhkalra96@gmail.com>
2018-01-31 13:26:24 +0530

    removed homepage module

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,7 +11,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.home),
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}, name='logout'),
     url(r'^register/$', views.register, name='register'),
