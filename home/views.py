24b7f4beebae399b1156ffb58dff98b50fc9a916
rishabh kalra <rishabhkalra96@gmail.com>
2018-01-31 16:04:23 +0530

    in continuation with previous commit

--- home/views.py
+++ home/views.py
@@ -18,3 +18,11 @@ class HomeView(TemplateView):
     def get(self, request):
         form = HomeForm()
         return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['post']
+
+            args = {'form': form, 'text': text}
+            return render(request, self.template_name, args)
