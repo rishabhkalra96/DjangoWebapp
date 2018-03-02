b8c8674ba1741825b322bc3ca3cf877a4fb8200d
rishabh kalra <rishabhkalra96@gmail.com>
2018-03-02 21:03:26 +0530

    minor changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -20,11 +20,11 @@ urlpatterns = [
     url(r'^reset-password/$', password_reset,
         {
             'template_name': 'accounts/reset_password.html',
-            'post_reset_redirect':'accounts:password_reset_done',
-            'email_template_name':'accounts/reset_password_email.html'
+            'post_reset_redirect': 'accounts:password_reset_done',
+            'email_template_name': 'accounts/reset_password_email.html'
         }, name='password_reset'),
 
-    url(r'^reset-password/done/$', password_reset_done,{'template_name':'accounts/reset_password_done.html'},
+    url(r'^reset-password/done/$', password_reset_done, {'template_name': 'accounts/reset_password_done.html'},
         name='password_reset_done'),
 
     url(r'^reset-password/confirm/$', password_reset_confirm, name='password_reset_confirm'),
@@ -32,12 +32,12 @@ urlpatterns = [
     url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
         password_reset_confirm,
         {
-            'template_name':'accounts/reset_password_confirm.html',
-            'post_reset_redirect':'accounts:password_reset_complete'
-        },name='password_reset_confirm'),
+            'template_name': 'accounts/reset_password_confirm.html',
+            'post_reset_redirect': 'accounts:password_reset_complete'
+        }, name='password_reset_confirm'),
 
     url(r'^password-reset/complete/$', password_reset_complete,
         {
-            'template_name':'accounts/reset_password_complete.html'
+            'template_name': 'accounts/reset_password_complete.html'
         }, name='password_reset_complete')
 ]
