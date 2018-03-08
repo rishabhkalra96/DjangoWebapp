a2da031bc4fdb3c5c07c0cbb981f7e5aa88ec938
rishabh kalra <rishabhkalra96@gmail.com>
2018-03-08 19:09:44 +0530

    deploying add_friend and lose_friend functions in the backend

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from home.views import HomeView
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='home')
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^connect/(?P<operation>.+)/(?P<pk>\d+)/$', views.change_friends, name='change_friends'),
 ]
--- home/views.py
+++ home/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
 from home.forms import HomeForm
-from home.models import Post
+from home.models import Post, Friend
 from django.contrib.auth.models import User
 
 
@@ -31,9 +31,19 @@ class HomeView(TemplateView):
             post = form.save(commit=False)
             post.user = request.user
             text = form.cleaned_data['post']
-
             post.save()
             return redirect('home:home')
         text = form.cleaned_data['post']
         args = {'form': form, 'text': text}
         return render(request, self.template_name, args)
+
+
+def change_friends(request, operation, pk):
+    new_friend = User.objects.get(pk=pk)
+
+    if operation == 'add':
+        Friend.make_friend(request.user,new_friend)
+    elif operation == 'remove':
+        Friend.lose_friend(request.user, pk=pk)
+
+    return redirect('home:home')
